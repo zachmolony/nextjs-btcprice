9c7e1a65a44571dfa25906e0bc4f9acf52b191af
zachmolony <31472522+zachmolony@users.noreply.github.com>
2020-04-23 10:33:42 +0100

    created navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,24 @@
 import Link from "next/link";
 
-const Navbar = () => (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">BTC Price Data</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+const Navbar = () => (        
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+            <img src="https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/bitoin_btc_coin_crypto-512.png" width="30" height="30" className="d-inline-block align-top" alt="" /> {' '}
+            BTC Price Monitor
+        </a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarColor02">
-        <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <Link to="/about"><a class="nav-link" href="#">About</a></Link>
-        </li>
+    <div className="collapse navbar-collapse" id="navbarColor03">
+        <ul className="navbar-nav mr-auto">
+            <li className={"nav-item "}>
+                <Link href="/"><a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a></Link>
+            </li>
+            <li className="nav-item">
+                <Link href="/about"><a className="nav-link" href="#">About</a></Link>
+            </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search" />
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-        </form>
     </div>
     </nav>
 )
