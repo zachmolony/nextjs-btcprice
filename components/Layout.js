3bfa6e851c44f339b45d0303f0940cde52f35bb7
zachmolony <31472522+zachmolony@users.noreply.github.com>
2020-04-23 10:33:23 +0100

    added bootstrap + axios call

--- components/Layout.js
+++ components/Layout.js
@@ -4,11 +4,13 @@ import Navbar from './Navbar';
 const Layout = (props) => (
     <div>
         <Head>
-            <title>Bitcoin price monitor</title>
-            <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-yrfSO0DBjS56u5M+SjWTyAHujrkiYVtRYh2dtB3yLQtUz3bodOeialO59u5lUCFF" crossorigin="anonymous" />
+            <title>BTC Monitor</title>
+            <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-yrfSO0DBjS56u5M+SjWTyAHujrkiYVtRYh2dtB3yLQtUz3bodOeialO59u5lUCFF" crossOrigin="anonymous" />
         </Head>
         <Navbar />
-        {props.children}
+        <div className="container">
+            {props.children}
+        </div>
     </div>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Layout from '../components/Layout';
+import Prices from '../components/Prices';
+import axios from 'axios'
+const Index = ({ bpi }) => {
+    return (
+        <Layout>
+            <div>
+                <h1>Latest Bitcoin Prices</h1>
+            </div>
+        </Layout>   
+    )
+}
 
-const Index = () => (
-    <Layout>
-        <div><h1>H</h1>Hello world.</div>
-    </Layout>
-)
+export async function getServerSideProps() {
+    const res = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+    return {
+        props: { bpi: res.data.bpi }
+    }
+}
 
 export default Index;
